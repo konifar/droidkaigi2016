cd33dcbdccdd5308107e302bfd1e69f7d82a5646
konifar <yahpeycoy0403@gmail.com>
2016-01-31 19:34:36 +0900

    Create social menu

--- app/src/main/java/com/konifar/confsched/fragment/AboutFragment.java
+++ app/src/main/java/com/konifar/confsched/fragment/AboutFragment.java
@@ -7,12 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.konifar.confsched.R;
 import com.konifar.confsched.databinding.FragmentAboutBinding;
 import com.konifar.confsched.util.AppUtil;
 
 public class AboutFragment extends Fragment {
 
     public static final String TAG = AboutFragment.class.getSimpleName();
+    private static final String REP_TWITTER_NAME = "mhidaka";
+    private static final String SITE_URL = "https://droidkaigi.github.io/2016";
+    private static final String CONF_TWITTER_NAME = "DroidKaigi";
+    private static final String CONF_FACEBOOK_NAME = "DroidKaigi";
 
     private FragmentAboutBinding binding;
 
@@ -29,7 +34,18 @@ public class AboutFragment extends Fragment {
     }
 
     private void initView() {
-        //
+        String repText = getString(R.string.about_rep, REP_TWITTER_NAME);
+        binding.txtRep.setText(repText);
+        AppUtil.linkify(getActivity(), binding.txtRep, REP_TWITTER_NAME, AppUtil.getTwitterUrl(REP_TWITTER_NAME));
+
+        binding.txtSiteUrl.setText(SITE_URL);
+        AppUtil.linkify(getActivity(), binding.txtSiteUrl, SITE_URL, SITE_URL);
+
+        binding.imgFacebookClicker.setOnClickListener(v ->
+                AppUtil.showWebPage(getActivity(), AppUtil.getFacebookUrl(CONF_FACEBOOK_NAME)));
+        binding.imgTwitterClicker.setOnClickListener(v ->
+                AppUtil.showWebPage(getActivity(), AppUtil.getTwitterUrl(CONF_TWITTER_NAME)));
+
         binding.txtVersion.setText(AppUtil.getVersionName(getActivity()));
     }
 
--- app/src/main/java/com/konifar/confsched/util/AppUtil.java
+++ app/src/main/java/com/konifar/confsched/util/AppUtil.java
@@ -1,11 +1,21 @@
 package com.konifar.confsched.util;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.konifar.confsched.R;
 
@@ -18,6 +28,7 @@ public class AppUtil {
 
     private static final String TWITTER_URL = "https://twitter.com/";
     private static final String GITHUB_URL = "https://github.com/";
+    private static final String FACEBOOK_URL = "https://www.facebook.com/";
 
     private static final String LANG_STRING_RES_PREFIX = "lang_";
     private static final String STRING_RES_TYPE = "string";
@@ -31,6 +42,10 @@ public class AppUtil {
 
     public static String getGitHubUrl(@NonNull String name) {
         return GITHUB_URL + name;
+    }
+
+    public static String getFacebookUrl(@NonNull String name) {
+        return FACEBOOK_URL + name;
     }
 
     public static void initLocale(Context context) {
@@ -100,6 +115,36 @@ public class AppUtil {
             Log.e(TAG, e.getMessage() + "");
             return "";
         }
+    }
+
+    public static void linkify(Activity activity, TextView textView, String linkText, String url) {
+        String text = textView.getText().toString();
+
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        builder.append(text);
+        builder.setSpan(
+                new ClickableSpan() {
+                    @Override
+                    public void onClick(View view) {
+                        showWebPage(activity, url);
+                    }
+                },
+                text.indexOf(linkText),
+                text.indexOf(linkText) + linkText.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+
+        textView.setText(builder);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    public static void showWebPage(Activity activity, @NonNull String url) {
+        CustomTabsIntent intent = new CustomTabsIntent.Builder()
+                .setShowTitle(true)
+                .setToolbarColor(ContextCompat.getColor(activity, R.color.theme500))
+                .build();
+
+        intent.launchUrl(activity, Uri.parse(url));
     }
 
 }
