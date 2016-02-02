ff62dd979e2be2c54c02a2bedbe74b352aed223f
konifar <yahpeycoy0403@gmail.com>
2016-02-03 03:23:04 +0900

    Added search result recycler view

--- app/src/main/java/io/github/droidkaigi/confsched/activity/SearchActivity.java
+++ app/src/main/java/io/github/droidkaigi/confsched/activity/SearchActivity.java
@@ -1,30 +1,41 @@
 package io.github.droidkaigi.confsched.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 import javax.inject.Inject;
 
 import io.github.droidkaigi.confsched.MainApplication;
 import io.github.droidkaigi.confsched.R;
 import io.github.droidkaigi.confsched.databinding.ActivitySearchBinding;
+import io.github.droidkaigi.confsched.databinding.ItemSearchResultBinding;
+import io.github.droidkaigi.confsched.model.SearchResult;
 import io.github.droidkaigi.confsched.util.AnalyticsUtil;
+import io.github.droidkaigi.confsched.widget.ArrayRecyclerAdapter;
+import io.github.droidkaigi.confsched.widget.BindingHolder;
 
 public class SearchActivity extends AppCompatActivity implements TextWatcher {
 
     private static final String TAG = SearchActivity.class.getSimpleName();
+    private static final int REQ_DETAIL = 1;
 
     @Inject
     AnalyticsUtil analyticsUtil;
+    @Inject
+    ActivityNavigator activityNavigator;
 
+    private SearchResultsAdapter adapter;
     private ActivitySearchBinding binding;
 
     static void start(@NonNull Activity activity) {
@@ -64,7 +75,10 @@ public class SearchActivity extends AppCompatActivity implements TextWatcher {
     }
 
     private void initRecyclerView() {
+        adapter = new SearchResultsAdapter(this);
 
+        binding.recyclerView.setAdapter(adapter);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
@@ -100,4 +114,29 @@ public class SearchActivity extends AppCompatActivity implements TextWatcher {
     public void afterTextChanged(Editable s) {
         // Do nothing
     }
+
+
+    private class SearchResultsAdapter extends ArrayRecyclerAdapter<SearchResult, BindingHolder<ItemSearchResultBinding>> {
+
+        public SearchResultsAdapter(@NonNull Context context) {
+            super(context);
+        }
+
+        @Override
+        public BindingHolder<ItemSearchResultBinding> onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new BindingHolder<>(getContext(), parent, R.layout.item_session);
+        }
+
+        @Override
+        public void onBindViewHolder(BindingHolder<ItemSearchResultBinding> holder, int position) {
+            SearchResult searchResult = getItem(position);
+            ItemSearchResultBinding binding = holder.binding;
+            binding.setSearchResult(searchResult);
+
+            binding.getRoot().setOnClickListener(v ->
+                    activityNavigator.showSessionDetail(SearchActivity.this, searchResult.session, REQ_DETAIL));
+        }
+
+    }
+
 }
