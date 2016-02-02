1bfffb6921e333b2ee9863cc06848cc73fcaad5f
konifar <yahpeycoy0403@gmail.com>
2016-02-03 03:01:27 +0900

    Created search result item layout

--- app/src/main/java/io/github/droidkaigi/confsched/model/SearchResult.java
+++ app/src/main/java/io/github/droidkaigi/confsched/model/SearchResult.java
@@ -16,25 +16,31 @@ public class SearchResult {
     @StringRes
     public int typeRes;
 
+    public String speakerImageUrl;
+
     public Session session;
 
-    private SearchResult(String text, int iconRes, int typeRes, Session session) {
+    private SearchResult(String text, int iconRes, int typeRes, String speakerImageUrl, Session session) {
         this.text = text;
         this.iconRes = iconRes;
         this.typeRes = typeRes;
+        this.speakerImageUrl = speakerImageUrl;
         this.session = session;
     }
 
     public static SearchResult createTitleType(@NonNull Session session) {
-        return new SearchResult(session.title, R.drawable.ic_event_note_grey_600_12dp, R.string.title, session);
+        return new SearchResult(session.title, R.drawable.ic_event_note_grey_600_12dp,
+                R.string.title, session.speaker.imageUrl, session);
     }
 
     public static SearchResult createDescriptionType(@NonNull Session session) {
-        return new SearchResult(session.description, R.drawable.ic_description_grey_600_12dp, R.string.description, session);
+        return new SearchResult(session.description, R.drawable.ic_description_grey_600_12dp,
+                R.string.description, session.speaker.imageUrl, session);
     }
 
     public static SearchResult createSpeakerType(@NonNull Session session) {
-        return new SearchResult(session.speaker.name, R.drawable.ic_person_grey_600_12dp, R.string.speaker, session);
+        return new SearchResult(session.speaker.name, R.drawable.ic_person_grey_600_12dp,
+                R.string.speaker, session.speaker.imageUrl, session);
     }
 
 }
