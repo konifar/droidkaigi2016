9002dc4de38407fb5407c3f307fb26ca42ed7a0f
konifar <yahpeycoy0403@gmail.com>
2016-02-04 05:33:44 +0900

    Improve back key confirm https://github.com/konifar/droidkaigi2016/issues/53

--- app/src/main/java/io/github/droidkaigi/confsched/activity/MainActivity.java
+++ app/src/main/java/io/github/droidkaigi/confsched/activity/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -34,12 +35,14 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int BACK_BUTTON_PRESSED_INTERVAL = 3000;
 
     @Inject
     AnalyticsUtil analyticsUtil;
 
     private ActivityMainBinding binding;
     private Fragment currentFragment;
+    private boolean isPressedBackOnce = false;
 
     static void start(@NonNull Activity activity) {
         Intent intent = new Intent(activity, MainActivity.class);
@@ -89,9 +92,20 @@ public class MainActivity extends AppCompatActivity
     public void onBackPressed() {
         if (binding.drawer.isDrawerOpen(GravityCompat.START)) {
             binding.drawer.closeDrawer(GravityCompat.START);
-        } else {
+        } else if (isPressedBackOnce) {
             super.onBackPressed();
+            return;
         }
+
+        isPressedBackOnce = true;
+        showSnackBar(getString(R.string.app_close_confirm));
+        new Handler().postDelayed(() -> isPressedBackOnce = false, BACK_BUTTON_PRESSED_INTERVAL);
+    }
+
+    private void showSnackBar(@NonNull String text) {
+        Snackbar.make(binding.getRoot(), text, Snackbar.LENGTH_LONG)
+                .setAction(R.string.app_close_now, v -> finish())
+                .show();
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
