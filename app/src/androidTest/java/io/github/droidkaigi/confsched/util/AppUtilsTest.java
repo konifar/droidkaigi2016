974477fbf3da2bf90f59a255bca0a83aace0ca30
gotokatsuya <peach921110@gmail.com>
2016-02-13 00:02:31 +0900

    Fix getCurrentLanguageId test

--- app/src/androidTest/java/io/github/droidkaigi/confsched/util/AppUtilsTest.java
+++ app/src/androidTest/java/io/github/droidkaigi/confsched/util/AppUtilsTest.java
@@ -7,6 +7,9 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(AndroidJUnit4.class)
@@ -25,7 +28,21 @@ public class AppUtilsTest {
     @Test
     public void testCurrentLanguageId() throws Exception {
         Context context = InstrumentationRegistry.getContext();
-        // AppUtil.getCurrentLanguageId does not return empty or null value.
-        assertThat(AppUtil.getCurrentLanguageId(context)).isNotNull().isNotEmpty();
+        // is not null value.
+        assertThat(AppUtil.getCurrentLanguageId(context)).isNotNull();
+
+        PrefUtil.put(context, PrefUtil.KEY_CURRENT_LANGUAGE_ID, "ja");
+        // eq to languageID is put in SharedPreferences.
+        assertThat(AppUtil.getCurrentLanguageId(context)).isEqualTo("ja");
+
+        PrefUtil.remove(context, PrefUtil.KEY_CURRENT_LANGUAGE_ID);
+        String defaultLanguage = Locale.getDefault().getLanguage().toLowerCase();
+        if (Arrays.asList(AppUtil.SUPPORT_LANG).contains(defaultLanguage)) {
+            // eq to Locale.getDefault().getLanguage() when it is supported
+            assertThat(AppUtil.getCurrentLanguageId(context)).isEqualTo(defaultLanguage);
+        } else {
+            // eq to "en" when Locale.getDefault().getLanguage() is not supported
+            assertThat(AppUtil.getCurrentLanguageId(context)).isEqualTo("en");
+        }
     }
 }
