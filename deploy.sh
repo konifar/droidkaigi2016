1b0871fb5a568e391abea4d4f4c3a0002c4d1409
konifar <yahpeycoy0403@gmail.com>
2016-02-13 12:30:08 +0900

    Fixed circle ci deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 GIT_HASH=`git rev-parse --short HEAD`
 
+./gradlew clean assembleProductionRelease
 curl -F "file=@app/build/outputs/apk/app-production-release.apk" -F "token=${DEPLOY_GATE_API_KEY}" -F "message=https://github.com/konifar/droidkaigi2016/tree/${GIT_HASH} https://circleci.com/gh/konifar/droidkaigi2016/${CIRCLE_BUILD_NUM}" -F "distribution_key=901884d8a27b8bd9c368109b0cc5c941a6821076" -F "release_note=https://github.com/konifar/droidkaigi2016/tree/${GIT_HASH} https://circleci.com/gh/konifar/droidkaigi2016/${CIRCLE_BUILD_NUM}" https://deploygate.com/api/users/konifar/apps
