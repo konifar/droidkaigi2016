c68eaeeb2981b35b903d0221cebfdcf2953aba3a
cutmail <sunday.is.over@gmail.com>
2016-02-13 13:32:04 +0900

    Delete unused import

--- app/src/main/java/io/github/droidkaigi/confsched/activity/SearchedSessionsActivity.java
+++ app/src/main/java/io/github/droidkaigi/confsched/activity/SearchedSessionsActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
 
 import org.parceler.Parcels;
