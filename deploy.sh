7f2ee5b0aa4e9b13acd008c851ab060c13c4d830
konifar <yahpeycoy0403@gmail.com>
2016-02-13 11:04:45 +0900

    Remove debug deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 GIT_HASH=`git rev-parse --short HEAD`
 
 # For α version. Remove this after 1.0.0 release.
-./gradlew clean assembleProductionDebug
-curl -F "file=@app/build/outputs/apk/app-production-debug.apk" -F "token=${DEPLOY_GATE_API_KEY}" -F "message=https://github.com/konifar/droidkaigi2016/tree/${GIT_HASH} https://circleci.com/gh/konifar/droidkaigi2016/${CIRCLE_BUILD_NUM}" -F "distribution_key=0d34ff6b3ccedf2a76771d9b6002c55b4f0aac1f" -F "release_note=https://github.com/konifar/droidkaigi2016/tree/${GIT_HASH} https://circleci.com/gh/konifar/droidkaigi2016/${CIRCLE_BUILD_NUM}" https://deploygate.com/api/users/konifar/apps
+# ./gradlew clean assembleProductionDebug
+# curl -F "file=@app/build/outputs/apk/app-production-debug.apk" -F "token=${DEPLOY_GATE_API_KEY}" -F "message=https://github.com/konifar/droidkaigi2016/tree/${GIT_HASH} https://circleci.com/gh/konifar/droidkaigi2016/${CIRCLE_BUILD_NUM}" -F "distribution_key=0d34ff6b3ccedf2a76771d9b6002c55b4f0aac1f" -F "release_note=https://github.com/konifar/droidkaigi2016/tree/${GIT_HASH} https://circleci.com/gh/konifar/droidkaigi2016/${CIRCLE_BUILD_NUM}" https://deploygate.com/api/users/konifar/apps
 
 # For β version. Remove comment out after first ci success.
 ./gradlew clean assembleProductionRelease
