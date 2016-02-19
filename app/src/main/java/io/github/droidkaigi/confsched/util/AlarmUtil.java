108222642d3df6d3e1a8cf77b07f086521d67707
ainame <s.namai.09@gmail.com>
2016-02-19 13:02:44 +0900

    Add alarm for finish notification

--- app/src/main/java/io/github/droidkaigi/confsched/util/AlarmUtil.java
+++ app/src/main/java/io/github/droidkaigi/confsched/util/AlarmUtil.java
@@ -10,11 +10,13 @@ import org.parceler.Parcels;
 import java.util.concurrent.TimeUnit;
 
 import io.github.droidkaigi.confsched.model.Session;
+import io.github.droidkaigi.confsched.receiver.SessionFinishReceiver;
 import io.github.droidkaigi.confsched.receiver.SessionScheduleReceiver;
 
 public class AlarmUtil {
 
     private static final long REMIND_DURATION_MINUTES_FOR_START = TimeUnit.MINUTES.toMillis(5);
+    private static final long REMIND_DURATION_MINUTES_FOR_FEEDBACK = TimeUnit.MINUTES.toMillis(5);
 
     public static void handleSessionAlarm(Context context, Session session) {
         if (session.checked) {
@@ -24,33 +26,50 @@ public class AlarmUtil {
         }
     }
 
-    public static void registerSessionAlarm(Context context, Session session) {
-        if (!session.shouldNotify(REMIND_DURATION_MINUTES_FOR_START)) {
-            return;
-        }
+    private static void registerSessionAlarm(Context context, Session session) {
 
-        PendingIntent sender = buildSessionAlarmSender(context, session);
 
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
-        long triggerAtMillis = calculateStartNotifyTime(session, context);
-        alarmManager.set(AlarmManager.RTC, triggerAtMillis, sender);
+        if (session.shouldNotify(REMIND_DURATION_MINUTES_FOR_START)) {
+            long triggerAtMillisForStart = calculateStartNotifyTime(session, context);
+            PendingIntent senderForStart = buildSessionStartAlarmSender(context, session);
+            alarmManager.set(AlarmManager.RTC, triggerAtMillisForStart, senderForStart);
+        
+
+        long triggerAtMillisForFeedback = calculateEndNotifyTime(session, context);
+        PendingIntent senderForFinish = buildSessionFinishAlarmSender(context, session);
+        alarmManager.set(AlarmManager.RTC, triggerAtMillisForFeedback, senderForFinish);
     }
 
-    public static void unregisterSessionAlarm(Context context, Session session) {
-        PendingIntent sender = buildSessionAlarmSender(context, session);
+    private static void unregisterSessionAlarm(Context context, Session session) {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        alarmManager.cancel(sender);
+
+        PendingIntent senderForStart = buildSessionStartAlarmSender(context, session);
+        alarmManager.cancel(senderForStart);
+
+        PendingIntent senderForFinish = buildSessionFinishAlarmSender(context, session);
+        alarmManager.cancel(senderForFinish);
     }
 
-    private static PendingIntent buildSessionAlarmSender(Context context, Session session) {
+    private static PendingIntent buildSessionStartAlarmSender(Context context, Session session) {
         Intent intent = new Intent(context, SessionScheduleReceiver.class);
         intent.putExtra(Session.class.getSimpleName(), Parcels.wrap(session));
         return PendingIntent.getBroadcast(context, session.id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
 
+    private static PendingIntent buildSessionFinishAlarmSender(Context context, Session session) {
+        Intent intent = new Intent(context, SessionFinishReceiver.class);
+        intent.putExtra(Session.class.getSimpleName(), Parcels.wrap(session));
+        int requestCode = -session.id; // use negated id to avoid conflict with start and finish
+        return PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     private static long calculateStartNotifyTime(Session session, Context context) {
         return session.getDisplaySTime(context).getTime() - REMIND_DURATION_MINUTES_FOR_START;
+    }
+
+    private static long calculateEndNotifyTime(Session session, Context context) {
+        return session.getDisplayETime(context).getTime() + REMIND_DURATION_MINUTES_FOR_FEEDBACK;
     }
 }
